ec96590514332018cb574cc090f58bacb150faf4
Parag Nandi <parag7.iitr@gmail.com>
2016-04-17 13:29:21 +0530

    Adding Red-Blue App files

--- Red - Blue/ViewController.swift
+++ Red - Blue/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var HideBlue: UIButton!
+    
+    @IBOutlet weak var HideRed: UIButton!
+    
+    @IBOutlet weak var BlueBomb: UIImageView!
+    
+    @IBOutlet weak var RedBomb: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func HideBlue(sender: AnyObject) {
+        BlueBomb.hidden = true
+    }
 
+    @IBAction func HideRed(sender: AnyObject) {
+        RedBomb.hidden = true
+    }
 }
 
